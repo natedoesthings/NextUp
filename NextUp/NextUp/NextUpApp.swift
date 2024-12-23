78e472278ab8b5c34eb4c87d5dac4f0bf34a1741
natedoesthings <nathanaeldtesfaye@gmail.com>
2024-12-22 22:12:41 -0500

    initial commit

---
+++ NextUp/NextUp/NextUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextUpApp.swift
+//  NextUp
+//
+//  Created by Nathanael Tesfaye on 12/22/24.
+//
+
+import SwiftUI
+
+@main
+struct NextUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
